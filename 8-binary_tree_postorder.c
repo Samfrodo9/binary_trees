25edc7721a8dd0a8322e4a2a337808732af53f5a
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-28 22:46:35 +0000

    Betty corrected

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,14 +6,14 @@
  * @func: A pointer to a function to call for each node. The value in the node
  *        must be passed as a parameter to this function.
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)) 
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL) 
-    {
-        return;
-    }
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
 
-    binary_tree_postorder(tree->left, func);
-    binary_tree_postorder(tree->right, func);
-    func(tree->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
