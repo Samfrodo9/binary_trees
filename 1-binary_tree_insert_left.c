198efdba7fd13473404b4dd62caa9715d0c08e63
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-28 19:23:32 +0000

    edited

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,7 +8,25 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
+
 	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+
+	return (new);
+
+	/** if (parent == NULL)
 		return (NULL);
 
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
@@ -30,4 +48,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	parent->left = newNode;
 
 	return (newNode);
+	**/
 }
