1a510ff6c98916e45d58ba3c701cef65c45fb7a5
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-28 18:38:39 +0000

    Left Child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_insert_left - A function to insert a node as left-child
  * @parent: pointer to the node to insert the left child in
  * @value: value to store in the new node
  * Return: NULL on failure or pointer to the newly created Node
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	if (parent == NULL)
