94db8a3034ca12db7d3f2a7b046c995f2efca7aa
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-28 22:24:24 +0000

    4 edited

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-#include <stddef.h>
-
-truct binary_tree_node{
-int data;
-struct binary_tree_node *left;
-struct binary_tree_node *right;
-};
-
-/**
-* binary_tree_is_leaf - Checks if a node is a leaf in a binary tree.
-* @node: A pointer to the node to check.
-* Return: 1 if the node is a leaf, 0 otherwise. If node is NULL, return 0.
-*/
-int binary_tree_is_leaf(const struct binary_tree_node *node)
-{
-if (node == NULL)
-{
-return (0);
-}
-
-return (((node->left == NULL) && (node->right == NULL)) ? 1 : 0);
-=======
 #include "binary_trees.h"
 /**
  * binary_tree_is_leaf - A function that deletes an entire binary tree
@@ -37,5 +14,4 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (0);
 	else
 		return (1);
->>>>>>> f275486c36fbf3526f262e5b2e8ac66d9a2a15ea
 }
