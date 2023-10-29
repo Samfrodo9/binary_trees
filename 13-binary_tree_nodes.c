da6e4d7298de4ac2c6c3d819e0a84acb74a61835
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-29 03:09:39 +0000

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,14 +8,13 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	size_t total_nodes = 0;
 
-	size_t left_nodes = binary_tree_nodes(tree->left);
-	size_t right_nodes = binary_tree_nodes(tree->right);
-
-	if (tree->left != NULL || tree->right != NULL)
-		return (1 + left_nodes + right_nodes);
-	else
-		return (left_nodes + right_nodes);
+	if (tree)
+	{
+		total_nodes += (tree->left || tree->right) ? 1 : 0;
+		total_nodes += binary_tree_nodes(tree->left);
+		total_nodes += binary_tree_nodes(tree->right);
+	}
+	return (total_nodes);
 }
