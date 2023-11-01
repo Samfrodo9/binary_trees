7c3b7ab4b90588f27d07b6abf1b2a6e945df02b6
Samfrodo9 <eungobs@gmail.com>
2023-11-01 10:00:09 -0700

    7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,20 +1,29 @@
 #include <stddef.h>
+#include <stdio.h>
 
-typedef struct binary_tree_node{
-    int data;
-    struct binary_tree_node *left;
-    struct binary_tree_node *right;
-} binary_tree_t;
-
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int)) 
+/**
+* binary_tree_inorder - Perform in-order traversal on a binary tree.
+* @tree: A pointer to the root node of the tree to traverse.
+* @func: A pointer to a function to call for each node. The value in the node
+*        must be passed as a parameter to this function.
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
+if (tree == NULL || func == NULL)
+{
+return;
+}
 
-    if (tree == NULL || func == NULL) 
-    {
-        return;
-    }
+binary_tree_inorder(tree->left, func);
+func(tree->data);
+binary_tree_inorder(tree->right, func);
+}
 
-    binary_tree_inorder(tree->left, func);
-    func(tree->data);
-    binary_tree_inorder(tree->right, func);
+/**
+* print_data - A sample function to print the data value of a binary tree node.
+* @data: The data value to be printed.
+*/
+void print_data(int data)
+{
+printf("%d ", data);
 }
